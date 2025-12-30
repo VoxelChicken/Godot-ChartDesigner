b21bdbfba2f8e7ecbb63940a322cc5aa93f1aa68
VoxelChicken <251214636+VoxelChicken@users.noreply.github.com>
2025-12-31 00:42:44 +0100

    Bar Chart bug fix The following changes have been made:
    Removed:
    - Removed Horizontal Bar Chart (Had lots of issues and I couldn't find a fix), altough it may be added in the future again
    Bug fixes:
    - The bar chart had a problem where the last bar (column) was slightly off-setted from the exact edge of the bar chart.

--- addons/chartdesigner/chart_designer.gd
+++ addons/chartdesigner/chart_designer.gd
@@ -4,14 +4,14 @@
 ## Follow up with a glance at the inspector - it has the variables required for designing the desired chart, and most importantly, it contains the [code]PackedFloat64Array[/code] for the points
 ## (in the inspector, the 'values' array isn't a [code]PackedVector2Array[/code], which the Godot's [code]Line2D[/code] uses, but rather a [code]PackedFloat64Array[/code]. This is because the only thing the user has to do is give the graph the values, because the regular increments on the x axis are done automatically calculated).
 ## The variables, apart from the [code]PackedFloat64Array[/code], include Color, Width, and Anti-aliasing. Tune these to your preference and you have yourself an easy chart, enjoy![br][br]
+## It is advisable to play around and change the variables at will to learn about each of the settings and what they change (additionally, nothing prevents you from glancing at the code, if you please).
 ## (Note: This is an open-source passion project by a solo developer. Updates are most likely not going to appear at regular intervals, but rather, at random.)
 class_name ChartDesigner
 extends Control
 
 enum ChartType{
 	LineChart,
-	BarChartVertical,
-	BarChartHorizontal,
+	BarChartVertical
 }
 
 ## This exported enum gives you a drop-down menu containing different chart types.
@@ -25,14 +25,17 @@ enum ChartType{
 @export var values: PackedFloat64Array
 
 ## With this color, you can choose which color you want the line to have.
-@export var line_color: Color
+@export var line_color: Color = Color.RED
 ## With this color you can choose your desired width of the x and y line's width. Setting it to -1 makes the line as thin as possible, in some cases, even less than 1 pixel wide.
-@export var x_y_lines_color: Color
+@export var x_y_lines_color: Color = Color.BLACK
 
 ## With this color you can choose your desired width of the line width. Setting it to -1 makes the line as thin as possible, in some cases, even less than 1 pixel wide.
-@export var line_width: float
+@export var line_width: float = 5
 ## This variable is the width of the background lines that indicate the x and y axes.
-@export var x_y_lines_width: float
+@export var x_y_lines_width: float = 10
+
+## With this variable for the bar charts exclusively, you can define the distance between the first and last bar (column) to the edge (y axis line).
+@export var distance_to_y_or_x: float
 
 ## This is the antialiasing [code] bool [/code]. It's also exported to the inspector. The antialiasing makes the pixelated lines smooth if '[code] True [/code]' ('On').
 @export var antialiasing: bool
@@ -90,7 +93,7 @@ func _draw() -> void:
 				Vector2(x_size, y_size - offset)
 			]
 			
-			var x_increment = (x_size) / (total_point_count - 1)
+			var x_increment = x_size / (total_point_count - 1)
 			
 			for i in total_point_count:
 				vector2_array[i] = Vector2(
@@ -101,37 +104,20 @@ func _draw() -> void:
 			draw_polyline(line_chart_x_y_indicator_point_array, x_y_lines_color, x_y_lines_width, antialiasing) # Draws the x and y helper lines.
 			
 		ChartType.BarChartVertical: # The code below runs when the user selected 'BarChartVertical' as the Chart Type.
-			var offset = x_y_lines_width / 2
+			var offset = x_y_lines_width / 2 # Makes the code nicer to look at
 			var line_chart_x_y_indicator_point_array: PackedVector2Array = [
 				Vector2(offset, 0),
 				Vector2(offset, y_size - offset),
 				Vector2(x_size, y_size - offset)
-			]
+			] # This array sets the point positions of the x and y axes lines.
 			
-			var x_increment = (x_size - line_width - (x_y_lines_width * 2)) / (total_point_count - 1)
-			
-			for i in total_point_count:
-					vector2_array[i] = Vector2(
-						x_increment * i + (line_width / 2) + x_y_lines_width, # x value
-						y_size - (y_size / max_val * values[i]) # y value
-						)
-					draw_line(vector2_array[i], Vector2(vector2_array[i].x, y_size), line_color, line_width, antialiasing) # Draws the main lines.
-			draw_polyline(line_chart_x_y_indicator_point_array, x_y_lines_color, x_y_lines_width, antialiasing) # Draws the x and y helper lines.
-			
-		ChartType.BarChartHorizontal: # The code below runs when the user selected 'BarChartHorizontal' as the Chart Type. This, for now, unfortunately works only from right-to-left horizontally, not left-to-right, as intended. The add-on is still in development, though!
-			var offset = x_y_lines_width / 2
-			var line_chart_x_y_indicator_point_array: PackedVector2Array = [
-				Vector2(x_size - offset, 0),
-				Vector2(x_size - offset, y_size - offset),
-				Vector2(offset, y_size - offset)
-			]
-			
-			var y_increment = (y_size - line_width - (x_y_lines_width * 2)) / (total_point_count - 1)
+			var x_increment = (x_size - line_width - distance_to_y_or_x) / (total_point_count - 1)
 			
 			for i in total_point_count:
 				vector2_array[i] = Vector2(
-					x_size - (x_size / max_val * values[i]), # x value
-					y_increment * i + (line_width / 2) + x_y_lines_width # y value
-				)
-				draw_line(vector2_array[i], Vector2(x_size, vector2_array[i].y), line_color, line_width, antialiasing) # Draws the main lines.
+					(x_increment * i) + (line_width / 2) + (distance_to_y_or_x / 2), # x value
+					y_size - (y_size / max_val * values[i]) # y value
+					)
+				draw_line(vector2_array[i], Vector2(vector2_array[i].x, y_size), line_color, line_width, antialiasing) # Draws the main lines.
 			draw_polyline(line_chart_x_y_indicator_point_array, x_y_lines_color, x_y_lines_width, antialiasing) # Draws the x and y helper lines.
+			
